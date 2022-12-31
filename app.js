1c971fa6ac8caf7453192291496b49514c7ed372
imma-nue7L-05 <immanuelchukwudubem5@gmail.com>
2022-12-31 18:59:17 +0100

    intro-component-with-signup-form-master - complete

---
+++ app.js
@@ -0,0 +1,20 @@
+const form = document.querySelector('.right-col form');
+const input = document.querySelectorAll('.right-col form input');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    input.forEach(input => {
+        if (!input.value) {
+            input.parentElement.classList.add('error');
+        } else {
+            input.parentElement.classList.remove('error');
+            if (input.type == 'email') {
+                if (validateEmail(input.value)) {
+                    input.parentElement.classList.remove('error');
+                } else {
+                    input.parentElement.classList.add('error');
+                }
+            }
+        }
+    });
+});
